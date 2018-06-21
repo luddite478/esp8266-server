41071a73d2ac2cab60a9c8535fd7217e1c572a8c
luddite478 <luddite478@gmail.com>
2018-06-21 20:34:59 +0300

    12

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+// const http = require('http').Server(app);
 // const io = require('socket.io')(http);
 let socket = null;
-
+const PORT = process.env.PORT || 5000
 app.use(express.static('views'))
 
 app.get('/', (req, res) => {
@@ -30,4 +30,4 @@ app.get('/released', (req, res) => {
 //
 // })
 
-http.listen(3000, () => console.log('port 3000'))
+app.listen(PORT, () => console.log('port 3000'))
