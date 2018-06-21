7fbbd1b8397a00c0c1e12cfd5d484091e9cec223
luddite478 <luddite478@gmail.com>
2018-06-21 20:36:36 +0300

    asd

--- views/scripts/main.js
+++ views/scripts/main.js
@@ -1,19 +1,19 @@
-console.log('gggggggggggggg');
-// const socket = io();
-//
-// socket.on('connection_established', () => {
-//   var timeInMs = new Date().toLocaleTimeString();
-//   console.log('connected : ' + timeInMs);
-// })
-//
-// socket.on('buttonState', (state) => {
-//
-//   if(state === 'pressed'){
-//     console.log('pressed');
-//   }
-//
-//   if(state === 'released'){
-//     console. log('released');
-//   }
-//
-// })
+console.log('started');
+const socket = io();
+
+socket.on('connection_established', () => {
+  var timeInMs = new Date().toLocaleTimeString();
+  console.log('connected : ' + timeInMs);
+})
+
+socket.on('buttonState', (state) => {
+
+  if(state === 'pressed'){
+    console.log('pressed');
+  }
+
+  if(state === 'released'){
+    console. log('released');
+  }
+
+})
